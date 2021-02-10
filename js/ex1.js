d3bc4f8b89d5262a4c55e9da1d4fcf06724f92c4
Stephanie Wells <sw.stephwells@gmail.com>
2021-02-09 18:20:48 -0800

    Testing page

---
+++ js/ex1.js
@@ -0,0 +1,14 @@
+var username = prompt("Please enter your name: ");
+
+var grade = parseInt(prompt("Please enter your number of units completed in college: "));
+
+if(grade<0){ console.log("Invalid entry. Please try again")}
+
+else{
+    console.log("Hello"+ username )
+    
+    if (grade >= 0 && grade <= 30 ) {console.log("Your grade standing is Freshman ")};
+    if (grade >= 31 && grade <= 60 ) {console.log("Your grade standing is Sophomore ")};
+    if (grade >= 61 && grade <= 90 ) {console.log("Your grade standing is Junior ")};
+    if (grade >= 91) {console.log("Your grade standing is Senior ")};
+}
---
+++ js/ex3.js
@@ -0,0 +1,11 @@
+var num1 = parseInt(prompt("Please enter an integer: "))
+
+var num2 = parseInt(prompt("Please enter another integer: "))
+
+
+
+console.log(`${num1} + ${num2} = ${num1 + num2}`)
+console.log(`${num1} - ${num2} = ${num1 - num2}`)
+console.log(`${num1} * ${num2} = ${num1 * num2}`)
+console.log(`${num1} * ${num2} = ${num1 * num2}`)
+console.log(`${num1} / ${num2} = ${num1 / num2}`)
